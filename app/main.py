abd1f81b84f2e17973f87aef86d3f72ef2017d9a
Bastien Boutonnet <bastien.b1@gmail.com>
2021-02-21 23:17:45 +0100

    feat: add route for table list and implement postgres connector

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 """API Entry Point"""
+from typing import List
+
 from fastapi import FastAPI
+from postgres_connector import PostgresConnector
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class GreetingModel(BaseModel):
+class SchemaRequest(BaseModel):
     """Pydantic model to validate request."""
+
+    schema_name: str
+
+
+class GreetingModel(BaseModel):
+    """Pydandict greeting validation model."""
 
     name: str
 
@@ -15,6 +24,12 @@ class GreetingResult(BaseModel):
     """Pydandict model to validate response."""
 
     message: str
+
+
+class ListOfTables(BaseModel):
+    """Pydandic list of  tables validation model."""
+
+    tables: List[str]
 
 
 @app.get("/")
@@ -28,3 +43,28 @@ def greet_by_name(request: GreetingModel) -> GreetingResult:
     """Takes a json with a name and returns a json with name greeted."""
     name = request.dict()["name"]
     return GreetingResult(**{"message": f"Hello Hello {name}"})
+
+
+@app.post("/tables", response_model=ListOfTables, name="Tables in Schema")
+def return_tables_in_schema(
+    request: SchemaRequest,
+) -> ListOfTables:
+    """Returns a list of tables given a schema name in the database.
+
+    Args:
+        request (SchemaRequest): json payload of shape e.g: {"schema_name": "public"}
+
+    Returns:
+        ListOfTables: json payload of shape {"tables": ["list", "of", "tables"]}
+    """
+    connector = PostgresConnector(
+        {
+            "host": "localhost",
+            "user": "anytool_user",
+            "password": "magical_password",
+            "database": "anytool_test_db",
+            "port": "5432",
+        }
+    )
+    tables = connector.get_tables_in_db_schema(target_schema=request.dict()["schema_name"])
+    return ListOfTables(**{"tables": tables})
--- app/postgres_connector.py
+++ app/postgres_connector.py
@@ -1,1 +1,26 @@
 """Holds Postgres DB Connector."""
+
+from typing import Dict
+
+import sqlalchemy
+from sqlalchemy import inspect
+
+
+class PostgresConnector:
+    """Postgres DB Connector/Adaptor."""
+
+    def __init__(self, connection_params: Dict[str, str]) -> None:
+        self.connection_url = sqlalchemy.engine.url.URL(
+            drivername="postgresql+psycopg2",
+            host=connection_params.get("host", str()),
+            username=connection_params.get("user", str()),
+            password=connection_params.get("password", str()),
+            database=connection_params.get("database", str()),
+            port=connection_params.get("port", str()),
+        )
+        self.engine = sqlalchemy.create_engine(self.connection_url)
+
+    def get_tables_in_db_schema(self, target_schema: str):
+        inspector = inspect(self.engine)
+        tables = inspector.get_table_names(schema=target_schema)
+        return tables
